62f6d5a6812f0cba90d16be98399769b97175c4a
Baek <Bjaeng@naver.com>
2024-10-15 18:49:36 +0900

    dd

--- client/src/infoPage.js
+++ client/src/infoPage.js
@@ -65,7 +65,7 @@ const Info = () => {
 
     const fetchBusInfo = async () => {
         try {
-            const response = await fetch(`http://localhost:8000/api/${direction}`);
+            const response = await fetch(`http://localhost:8000/bus/${direction}`);
             if (!response.ok) {
                 throw new Error('네트워크 오류입니다.');
             }
