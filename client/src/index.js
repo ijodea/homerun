924afbede14b203e12977dc9010bc1001851c3b4
seung hyeon <shyeeoon@gmail.com>
2024-12-08 19:07:39 +0900

    탭 수정

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+document.title = "HOMERUN";
+
+const changeFavicon = (iconURL) => {
+  const link = document.querySelector("link[rel~='icon']");
+  if (link) {
+    link.href = iconURL;
+  } else {
+    const newLink = document.createElement("link");
+    newLink.rel = "icon";
+    newLink.href = iconURL;
+    document.head.appendChild(newLink);
+  }
+};
+
+changeFavicon("/icon.png");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +24,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
